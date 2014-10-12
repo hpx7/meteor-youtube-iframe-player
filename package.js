5f863e55ad4e6696ca0eae03cfaabb7c32c2a9aa
Harsh Pandey <hpandey793@gmail.com>
2014-10-12 01:23:25 -0400

    finishing package.js

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
-  name: "hpx7:youtube-iframe-player",
-  summary: " \* Fill me in! *\ ",
-  version: "0.1.0",
-  git: "https://github.com/hpx7/meteor-youtube-iframe-player.git"
+  name: 'hpx7:youtube-iframe-player',
+  summary: 'Control YouTube\'s embedded player using Meteor',
+  version: '0.1.0',
+  git: 'https://github.com/hpx7/meteor-youtube-iframe-player.git'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.3.1');
+  api.use(['underscore', 'jquery', 'reactive-var'], 'client');
   api.addFiles('yt.js');
 });
